39d3473de7966d6a9aee0c99ed81e84fc0686a94
sumanguha1994 <suman.sgn@gmail.com>
2020-08-12 23:23:28 +0530

    update:Ci rest api controller/library/config file added

---
+++ application/controllers/ApiCtrl.php
@@ -0,0 +1,33 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+class ApiCtrl extends REST_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    /**
+     * Get All Data from this method.
+     *
+     * @return Response
+    */
+    public function index_get($id = 0)
+	{
+        if(!empty($id)){
+            $data = $this->db->get_where("items", ['id' => $id])->row_array();
+        }else{
+            $data = $this->db->get("items")->result();
+        }
+        $this->response($data, REST_Controller::HTTP_OK);
+    }
+
+    public function index_post()
+    {
+        $input = $this->input->post();
+        $this->db->insert('items',$input);
+        $this->response(['Item created successfully.'], REST_Controller::HTTP_OK);
+    } 
+}
