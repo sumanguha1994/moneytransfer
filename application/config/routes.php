a0ccbf5654cc3d593929ba7c293aa9471a776682
sumanguha1994 <suman.sgn@gmail.com>
2020-08-21 10:24:44 +0530

    fixed:all routes change post to get, controller, model #changed

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,12 +38,13 @@ $route['api/update-userdata'] = 'ApiCtrl/index_get';
 $route['api/receiver-details'] = 'ApiCtrl/index_get';
 
 #############################################################################################################
-# http://localhost/moneytransfer/api/signup             --post-- [signup|name|adharno|mobileno|yourid]      #
-# http://localhost/moneytransfer/api/login              --post-- [login|mobileno|yourid]                    #
-# http://localhost/moneytransfer/api/payment            --post-- [payment|sid|rname|rmobile|ramount|rtoken] #
-# http://localhost/moneytransfer/api/get-wallet         --get--  [wallet|uid]                               #
-# http://localhost/moneytransfer/api/get-alllog         --get--  [alllog|uid]                               #
-# http://localhost/moneytransfer/api/get-log            --get--  [log|uid]                                  #
-# http://localhost/moneytransfer/api/get-userdata       --get--  [userdata|uid]                             #
-# http://localhost/moneytransfer/api/update-userdata    --post-- [update|name|yourid|conf_yourid]           #
+# http://localhost/moneytransfer/api/signup             --get-- [signup|name|adharno|mobileno|yourid]       #
+# http://localhost/moneytransfer/api/login              --get-- [login|mobileno|yourid]                     #
+# http://localhost/moneytransfer/api/payment            --get-- [payment|sid|rname|rmobile|ramount|rtoken]  #
+# http://localhost/moneytransfer/api/get-wallet         --get-- [wallet|uid]                                #
+# http://localhost/moneytransfer/api/get-alllog         --get-- [alllog|uid]                                #
+# http://localhost/moneytransfer/api/get-log            --get-- [log|uid]                                   #
+# http://localhost/moneytransfer/api/get-userdata       --get-- [userdata|uid]                              #
+# http://localhost/moneytransfer/api/update-userdata    --get-- [update|name|yourid|conf_yourid]            #
+# http://localhost/moneytransfer/api/receiver-details   --get-- [receive|rtoken]                            #
 #############################################################################################################
